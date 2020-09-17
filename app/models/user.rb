743c4314199f0bff9701d9343fb4dd2b193ef710
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-17 17:02:07 +0900

    モデル、コントローラーの生成、モデルアソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_one_attached :image
+  has_one_attached :image
+  has_many :contents, dependent: :destroy
 
   validates :nickname, presence: true
 end
