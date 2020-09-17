7ab5a3f4c6fbd603e5a2fb5a54c893d98ffe9b8e
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-17 19:31:31 +0900

    コンテンツ投稿機能実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,11 +3,23 @@ class ContentsController < ApplicationController
   end
 
   def new
+    @content = Content.new
+  end
+
+  def create
+    @content = Content.new(content_params)
+    @content.save
+    redirect_to content_path(@content)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def content_params
+    params.require(:content).permit(:title, :note, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users, only:[:index, :edit, :show, :update]
-  resources :contents, only:[:index, :new, :edit, :show, :update]
+  resources :contents
 end
