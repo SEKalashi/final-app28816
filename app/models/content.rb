8cdc437211929aff231ca864330cd8536cd2bec5
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-19 17:57:02 +0900

    contentモデルのバリデーション設定、データ編集成否による画面遷移を設定を修正

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,7 +2,7 @@ class Content < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options precense: true do
+  with_options presence: true do
     validates :title
     validates :note
     validates :image
