9641180e6cdc4f9f20357e0eee8964dc03933a90
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-23 14:38:25 +0900

    投稿title検索バーを実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   resources :users, only:[:index, :edit, :show, :update, :destroy]
-  resources :contents
+  resources :contents do
+    collection do
+      get 'search'
+    end
+  end
 end
